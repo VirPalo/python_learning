b5f666b9eb3eb9b6de6a26eaf98b35f6b73b17a2
Virginia Paloma <virginiapaloma92@gmail.com>
2025-12-05 13:04:11 -0300

    ejercicio1 completo

---
+++ ejercicios/ejercicio1_variables.py
@@ -0,0 +1,19 @@
+'''
+Ejercicio 1: Variables y tipos de datos
+Autora: Virginia Paloma
+Año: 2025
+'''
+
+# 5 Variables de diferentes tipos
+nombre = 'Virginia'
+edad = 33
+altura = 1.68
+es_ingeniera = True
+ciudades_visitadas = ['Rosario', 'Buenos Aires', 'Córdoba', 'Mendoza']
+
+# Mostrar resultados
+print(f'Nombre: {nombre} - Tipo de dato: {type(nombre)}')
+print(f'Edad: {edad} años - Tipo de dato: {type(edad)}')
+print(f'Altura: {altura} metros - Tipo de dato: {type(altura)}')
+print(f'Es ingeniera: {es_ingeniera} - Tipo de dato: {type(es_ingeniera)}')
+print(f'Ciudades visitadas {ciudades_visitadas} - Tipo de dato: {type(ciudades_visitadas)}')
